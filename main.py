8fa74294442c29ce7464c1cfc40623ed8600f7e4
getmany1@aalto.fi <getmany1@aalto.fi>
2021-05-10 17:00:40 +0300

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
-import speech_recognition as sr
 from  fluency import calc_fluency
 from werkzeug.utils import secure_filename
 import glob, os
 import random
+from utils import *
 
 
-for f in glob.glob("upload/*"):
-    os.remove(f)
+# for f in glob.glob("upload/*"):
+#     os.remove(f)
 
 ALLOWED_EXTENSIONS = {'wav'}
 
@@ -16,12 +16,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    transcript = ""
+    transcript_kaldi = ""
+    transcript_wav2vec = ""
     fluency_info = ""
     task_achievement = ""
     pronunciation = ''
     range = ""
     accuracy = ""
+
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
+    w2v_model = Wav2Vec2ForCTC.from_pretrained('wav2vec2-large-100K-digitala-freeform').to(device)
+    w2v_processor = Wav2Vec2Processor.from_pretrained("wav2vec2-large-100K-digitala-freeform")
+    kaldi_path='digitala-freeform-kaldi'
+
     if request.method == "POST":
         print("FORM DATA RECEIVED")
 
@@ -36,11 +44,14 @@ def index():
         if file.filename == "":
             return redirect(request.url)
         
-        r = sr.Recognizer()
-        with sr.AudioFile(f'upload/{filename}') as source:
-            audio = r.record(source)  # read the entire audio file
+        # r = sr.Recognizer()
+        # with sr.AudioFile(f'upload/{filename}') as source:
+        #     audio = r.record(source)  # read the entire audio file
 
-        transcript = r.recognize_google(audio, show_all=False, language="fi")
+        # transcript = r.recognize_google(audio, show_all=False, language="fi")
+        audio, sr = librosa.load(os.path.join('upload',filename))
+        transcript_kaldi, transcript_wav2vec = asr(kaldi_path=kaldi_path, w2v_model=w2v_model, \
+            w2v_processor=w2v_processor, audio=audio, sr=sr)
 
         fluency_info = calc_fluency()
         task_achievement = f" {random.choice([1,2,3])} out of 3 (unreal score)"
@@ -48,8 +59,10 @@ def index():
         accuracy = f"{random.choice([1,2,3])} out of 3 (unreal score)"
         pronunciation = f"{random.choice([1,2,3])} out of 3 (unreal score)"
 
-
-    return render_template('index.html', transcript=transcript, \
+        print("KALDI",transcript_kaldi)
+        print("WAV2VEC2",transcript_wav2vec)
+    return render_template('index.html', transcript_kaldi=transcript_kaldi, \
+        transcript_wav2vec=transcript_wav2vec, \
         fluency=fluency_info, taskAchievement=task_achievement, range=range, \
             accuracy=accuracy, pronunciation=pronunciation)
 
