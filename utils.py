77efa566cd898631d7561e9128fa3b100ca60653
getmany1@aalto.fi <getmany1@aalto.fi>
2021-05-12 10:00:30 +0300

    fixed bug with audio sampling rate

--- utils.py
+++ utils.py
@@ -11,11 +11,11 @@ import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 
 def extract_words(res):
-   jres = json.loads(res)
-   if not 'result' in jres:
-       return []
-   words = jres['result']
-   return words
+    jres = json.loads(res)
+    if not 'result' in jres:
+        return []
+    words = jres['result']
+    return words
 
 def transcribe_words(recognizer, bytes):
     results = []
@@ -35,7 +35,7 @@ def transcribe_words(recognizer, bytes):
 def asr_wav2vec(audio, sr, model, processor):
     #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     device = torch.device('cpu')
-    model = model.to(device)
+    #model = model.to(device)
     input_values = processor(audio, sampling_rate=16000, return_tensors="pt", padding="longest").input_values
     with torch.no_grad():
         logits = model(input_values.to(device)).logits
@@ -55,12 +55,14 @@ def asr_kaldi(kaldi_path, audio, sr):
 
 def asr(kaldi_path, w2v_model, w2v_processor, audio=None, sr=None):
     if audio is None and sr is None:
-      audio, sr = get_audio()
+        print('Error! No audio loaded!')
+        #audio, sr = get_audio() #a function for recording audio
     elif type(audio) is str:
-      audio, sr = librosa.load(audio)
+        audio, sr = librosa.load(audio, sr=16000)
     if sr != 16000:
-      audio = librosa.resample(np.array(audio/32767.0, dtype=np.float32), sr, 16000)
-      sr = 16000
+        #audio = librosa.resample(np.array(audio/32767.0, dtype=np.float32), sr, 16000)
+        audio = librosa.resample(audio, sr, 16000)
+        sr = 16000
     transcript_kaldi=asr_kaldi(kaldi_path=kaldi_path, audio=audio, sr=sr)
     transcript_wav2vec=asr_wav2vec(audio=audio, sr=sr, model=w2v_model, processor=w2v_processor)
     return transcript_kaldi, transcript_wav2vec
